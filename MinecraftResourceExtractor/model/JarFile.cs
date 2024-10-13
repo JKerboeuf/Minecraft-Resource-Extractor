669cf2f531e2a1a4c9cdbb10ae3f019c08b65b7c
Julien Kerboeuf <Julien.Kerboeuf@gmail.com>
2024-10-13 07:44:29 +0200

    fixed unsafe dependancy

--- MinecraftResourceExtractor/model/JarFile.cs
+++ MinecraftResourceExtractor/model/JarFile.cs
@@ -1,5 +1,4 @@
 ﻿using mre.view;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
--- MinecraftResourceExtractor/view/FrmAbout.Designer.cs
+++ MinecraftResourceExtractor/view/FrmAbout.Designer.cs
@@ -97,7 +97,7 @@
 			this.lblVersion.Name = "lblVersion";
 			this.lblVersion.Size = new System.Drawing.Size(60, 13);
 			this.lblVersion.TabIndex = 6;
-			this.lblVersion.Text = "Version 2.1";
+			this.lblVersion.Text = "Version 2.2";
 			// 
 			// lblCreator
 			// 
